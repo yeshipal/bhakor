c86881c0855cbb7c9d7edccd4f091f550a549b70
Yeshi Paljor <92138464+yeshipal@users.noreply.github.com>
2025-07-04 14:22:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
           alert("You didn't enter a name.");
         }
       }
-
 const map = L.map('map').setView([28.3949, 84.1240], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
