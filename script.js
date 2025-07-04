bfad621d76663ab2c86f456d94591afd91714774
Yeshi Paljor <92138464+yeshipal@users.noreply.github.com>
2025-07-04 14:12:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// This function runs when the button is clicked
+      function greetUser() {
+        let name = prompt("What's your name? \n ཁྱེད་རང་གི་མིང་ལ་ག་རེ་རེད།");
+        if (name) {
+          alert("Tashi Delek བཀྲ་ཤིས་བདེ་ལེགས།, " + name + "! 👋");
+        } else {
+          alert("You didn't enter a name.");
+        }
+      }
